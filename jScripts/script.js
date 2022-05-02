c9f28c0c68f99b95780c7515b22d17a7cfda2510
sapir98 <64593535+sapir98@users.noreply.github.com>
2022-05-02 20:04:46 +0300

    Add files via upload

--- jScripts/script.js
+++ jScripts/script.js
@@ -1,25 +1,9 @@
-var showCookiesManagement = false;
-
-$('document').ready(function () {
-    showCookiesManagement = false
-    $("#cookies-management").hide("fast");
-});
-
 $(function () {
     $("#cookies-dialog").dialog({
         autoOpen: true,
         buttons: {
             "אישור העוגיות": function () {
                 $(this).dialog("close")
-            },
-            "ניהול  עוגיות": function () {
-                if (showCookiesManagement) {
-                    $("#cookies-management").hide("fast");
-                    showCookiesManagement = false;
-                } else {
-                    $("#cookies-management").show("fast");
-                    showCookiesManagement = true;
-                }
             }
         },
         hide: "puff",
@@ -27,6 +11,7 @@ $(function () {
         width: 600,
         classes: {
             'ui-dialog-titlebar-close': 'hidden',
-        }
+        },
+        modal: true
     })
 });
